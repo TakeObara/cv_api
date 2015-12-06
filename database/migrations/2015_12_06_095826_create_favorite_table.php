593bfb7fcb789606a4918d8fca59d1c85a540c51
TakeObara <bararararatty@gmail.com>
2015-12-06 22:21:28 +0900

    typo

--- database/migrations/2015_12_06_095826_create_favorite_table.php
+++ database/migrations/2015_12_06_095826_create_favorite_table.php
@@ -13,7 +13,7 @@ class CreateFavoriteTable extends Migration
     public function up()
     {
         Schema::create('favorite', function (Blueprint $table) {
-            $table->increment('id');
+            $table->increments('id');
             $table->integer('user_id');
             $table->integer('favorite_user_id');
             $table->timestamps();
