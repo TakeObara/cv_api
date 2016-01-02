1ee2427f413ba191881d5dab74520b003c01d940
edisonthk-mac <edisonthk@gmail.com>
2016-01-03 06:41:26 +0900

    add messages component

---
+++ resources/react/components/messages.js
@@ -0,0 +1,79 @@
+import UserAction from "../actions/userAction"
+import UserStore from "../stores/userStore"
+
+import AppointmentStore from "../stores/appointmentStore";
+import AppointmentAction from "../actions/appointmentAction";
+
+
+export default class MessagesList extends React.Component {
+
+    constructor() {
+        super();
+
+        UserAction.loadMyProfile();
+        // AppointmentAction.loadAll();
+
+        this.state = {
+            me: UserStore.getMyProfile(),
+            // list: AppointmentStore.getAll(),
+        };
+
+        this._onUserChange = this._onUserChange.bind(this);
+        // this._onChange     = this._onChange.bind(this)
+    }
+
+
+    componentDidMount() {
+        UserStore.addChangeListener(this._onUserChange);
+        // AppointmentStore.addChangeListener(this._onChange);
+    }
+
+    componentWillUnmount() {
+        UserStore.removeChangeListener(this._onUserChange);
+        // AppointmentStore.removeChangeListener(this._onChange);
+    }
+
+    _onUserChange() {
+        console.log("favourite component in _onUserChange");
+        var _state = this.state;
+        _state.me = UserStore.getMyProfile();
+        this.setState(_state);
+    }
+
+    // _onChange() {
+    //     console.log("favourite component in _onChange");
+    //     var _state = this.state;
+    //     _state.list = AppointmentStore.getAll();
+    //     this.setState(_state);
+    // }
+
+    render() {
+
+        var profileCoverStyle = { backgroundImage: 'url('+this.state.me.profile_image_url+')' };
+
+        // var list = [];
+        // for (var i = 0; i < this.state.list.length; i++) {
+        //     var _favour = this.state.list[i];
+        //     list.push(
+        //         <div key={i} className="clearfix favouriteItem">
+        //             <img className="profileImg" src={_favour.profile.profile_image_url}/>
+        //             <div className="profileMeta">
+        //                 <p>{_favour.profile.name}</p>
+        //                 <p>{_favour.profile.description}</p>
+        //             </div>
+        //         </div>
+        //     );
+        // };
+
+        return (
+            <div className="halfPage">
+                <div className="halfPage-cover profile-cover" style={profileCoverStyle}></div>
+                <div className="halfPage-cover dark-cover"></div>
+                <div className="content">
+                    <div className="halfPage-title">MESSAGES</div>
+                </div>
+            </div>
+        );
+    }
+}
+
--- resources/react/route.js
+++ resources/react/route.js
@@ -1,5 +1,6 @@
 import Main from "./components/main" 
 import Profile from "./components/profile" 
+import MessagesList from "./components/messages"
 import Favourite from "./components/favourite" 
 import Appointment from "./components/appointment" 
 import Info from "./components/info"
@@ -19,7 +20,7 @@ var routesMap = (
         <Route path="/" component={Main}>
             <Route path="profile" component={Profile} />
             <Route path="favourite" component={Favourite} />
-            <Route path="messages" component={Profile} />
+            <Route path="messages" component={MessagesList} />
             <Route path="appointment" component={Appointment} />
             <Route path="info" component={Info} />
         </Route>
