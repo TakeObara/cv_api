bf6537b71ee80e323303e3bbb710256442f66a6b
TakeObara <bararararatty@gmail.com>
2016-02-04 02:54:35 +0900

    add UserTableSeeder &  ChatroomMessageTableSeeder

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Database\Seeder;
 
+use Cv\Model\User;
+use Cv\Model\Profile;
+
 class UserTableSeeder extends Seeder
 {
     public function __construct(
@@ -18,7 +21,7 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $user_tmp = [];
         $users = [
             ['name' => 'test1', 'email' => 'test1@gmail.com', 'password' => '1234', 'gender' => '1', 'is_public' => true],
             ['name' => 'test2', 'email' => 'test2@gmail.com', 'password' => '1234', 'gender' => '1', 'is_public' => true],
@@ -27,8 +30,22 @@ class UserTableSeeder extends Seeder
             ['name' => 'test5', 'email' => 'test5@gmail.com', 'password' => '1234', 'gender' => '1', 'is_public' => true],
         ];
 
+        DB::table('users')->delete();
+        DB::table('profiles')->delete();
+           
+        $loop = 1;
         foreach ($users as $user){
             $this->auth->registerUser($user['name'], $user['email'], $user['password'], $user['gender'], null, null, '', $user['is_public']);
+            $this->changeIds($user['name'], $loop++);
         }
+    }
+
+    private function changeIds($userName, $id){
+        $user = User::where('name', "=", $userName)->first();
+        $profile = Profile::where("user_id", "=", $user['id'])->first();
+        $user->id = $id;
+        $profile->user_id = $id;
+        $user->save();
+        $profile->save();
     }
 }
